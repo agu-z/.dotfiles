b24883ce4038278c7784292b7fb4c5f0668be283
Agustín Zubiaga <aguzubiaga97@gmail.com>
2017-04-24 10:52:50 -0300

    update stuff

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -15,7 +15,7 @@ tm_separator_right_thin="❯"
 
 set -g status-left-length 32
 set -g status-right-length 150
-set -g status-interval 5
+set -g status-interval 60
 
 
 # default statusbar colors
@@ -49,10 +49,11 @@ set-option -g display-panes-colour $tm_color_inactive
 # clock
 set-window-option -g clock-mode-colour $tm_color_active
 
-tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/now-playing-radiant-player.scpt)"
+#tm_tunes="#[fg=$tm_color_music]#(osascript ~/.dotfiles/applescripts/now-playing-radiant-player.scpt)"
+tm_tunes=""
 tm_battery="#(~/.dotfiles/tmux/battery_indicator.sh)"
 
 tm_date="#[fg=$tm_color_inactive] %R %d %b"
 
-set -g status-left "🍕 $tm_session_name'"
-set -g status-right $tm_tunes' '$tm_date' '$tm_battery' ' 
+set -g status-left "🍕 $tm_session_name"
+set -g status-right $tm_tunes' '$tm_date' '$tm_battery' '
